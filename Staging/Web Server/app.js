eef36065ed3a6a5211c52968796a92f1c0464a1b
SofasInspector <r7e.games@gmail.com>
2023-09-29 20:23:57 +1000

    disabled auth again removed http status codes (simplifying code)

--- Staging/Web Server/app.js
+++ Staging/Web Server/app.js
@@ -24,17 +24,17 @@ var FFTemplate = '0'
 
 //check
 app.get('/ff/JOATWS', async (req, res) => {
-  if (req.Authorization == 'supersecreatpassword') {
+  if (req.Authorization = 'supersecreatpassword') {
       //authed
       res.send(FFJOATWebServer)
     } else {
       //auth incorrect
-      res.sendstatus(401).send('ERROR 401 Authentication requiried')
+      res.send('ERROR 401 Authentication requiried')
     }})
 
 //toggle
 app.get('/ff/JOATWS/toggle', async (req, res) => {
-  if (req.Authorization == 'supersecreatpassword') {
+  if (req.Authorization = 'supersecreatpassword') {
       //authed
 
       //toggle
@@ -48,7 +48,7 @@ app.get('/ff/JOATWS/toggle', async (req, res) => {
        console.log('JOATWS Toggled')
      } else {
        //auth incorrect
-      res.sendstatus(401).send('ERROR 401 Authentication requiried')
+      res.send('ERROR 401 Authentication requiried')
     }})
 
 
@@ -57,21 +57,21 @@ app.get('/ff/JOATWS/toggle', async (req, res) => {
 //check
 app.get('/ff/template', async (req, res) => {
   if (FFJOATWebServer = '1') {
-    if (req.Authorization == 'supersecreatpassword') {
+    if (req.Authorization = 'supersecreatpassword') {
       //authed
       res.send(FFTemplate)
     } else {
       //auth incorrect
-      res.sendstatus(401).send('ERROR 401 Authentication requiried')
+      res.send('ERROR 401 Authentication requiried')
     }
   } else {
-    res.sendstatus(503).send('Feature Flag for JOAT Web Server Disabled')
+    res.send('Feature Flag for JOAT Web Server Disabled')
   }})
 
 //toggle
 app.get('/ff/template/toggle', async (req, res) => {
   if (FFJOATWebServer = '1') {
-    if (req.Authorization == 'supersecreatpassword') {
+    if (req.Authorization = 'supersecreatpassword') {
       //authed
 
       //toggle
@@ -85,10 +85,10 @@ app.get('/ff/template/toggle', async (req, res) => {
        console.log('Template Toggled')
      } else {
        //auth incorrect
-      res.sendstatus(401).send('ERROR 401 Authentication requiried')
+      res.send('ERROR 401 Authentication requiried')
     }
   } else {
-    res.sendstatus(503).send('Feature Flag for JOAT Web Server Disabled')
+    res.send('Feature Flag for JOAT Web Server Disabled')
   }})
 
 
