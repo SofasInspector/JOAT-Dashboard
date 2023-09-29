0afa3a3f569f6640b0e83354c1d6dc4e2a562acf
SofasInspector <r7e.games@gmail.com>
2023-09-29 21:17:52 +1000

    actually made it mostly work

--- Staging/Static Site/index.js
+++ Staging/Static Site/index.js
@@ -7,21 +7,31 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     // Function to update checkbox state based on server response
-    function updateCheckboxState(checkboxElement, apiUrl) {
-        axios.get(url + apiUrl)
-            .then(response => {
-                checkboxElement.checked = (parseInt(response.data, 10) === 1);
-            })
+    async function updateCheckboxState(checkboxElement, apiUrl) {
+        try {
+            const response = await axios.get(url + apiUrl);
+            if (parseInt(response.data, 10) == 1) {
+                checkboxElement.checked = true;
+            } else {
+                checkboxElement.checked = false;
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     // Function to handle checkbox change event
     function handleCheckboxChange(checkboxElement, apiUrl) {
-        checkboxElement.addEventListener('change', function() {
-            axios.get(url + apiUrl + '/toggle', {
-                params: {
-                    Authorization: 'supersecretpassword',
-                }
-            })
+        checkboxElement.addEventListener('change', async function() {
+            try {
+                await axios.get(url + apiUrl + '/toggle', {
+                    params: {
+                        Authorization: 'supersecretpassword',
+                    }
+                });
+            } catch (error) {
+                console.error(error);
+            }
         });
     }
 
@@ -33,4 +43,15 @@ document.addEventListener('DOMContentLoaded', function() {
             handleCheckboxChange(checkboxElement, apiUrl);
         }
     }
-});
+
+    const updateButton = document.getElementById('updateButton');
+    updateButton.addEventListener('click', async function() {
+        for (const checkboxName in checkboxes) {
+            if (checkboxes.hasOwnProperty(checkboxName)) {
+                const apiUrl = checkboxes[checkboxName];
+                const checkboxElement = document.getElementById(checkboxName);
+                await updateCheckboxState(checkboxElement, apiUrl);
+            }
+        }
+    })
+})
